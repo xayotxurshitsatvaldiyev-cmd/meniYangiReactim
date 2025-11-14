bf998306bae4e1e559d964ae58b7fd41b52c9596
xayotxurshitsatvaldiyev-cmd <xayotxurshitsatvaldiyev@gmail.com>
2025-11-14 17:25:55 +0500

    Refactor image imports in about2.jsxchanged imgs source

--- src/about2/about2.jsx
+++ src/about2/about2.jsx
@@ -1,11 +1,12 @@
 import "./about2.css"
-import image from "./src/clientlar/image9.png"
-import c1 from "./src/about2/clientlarImgs/client1.png"
-import c2 from "./src/about2/clientlarImgs/client2.png"
-import c3 from "./src/about2/clientlarImgs/client3.png"
-import c4 from "./src/about2/clientlarImgs/client4.png"
-import c5 from "./src/about2/clientlarImgs/client5.png"
-import c6 from "./src/about2/clientlarImgs/client6.png"
+// import image from "./src/about2/clientlar/image9.png"
+import image from "../about2/image9.png"
+import c1 from "../about2/clientlarImgs/client1.png"
+import c2 from "../about2/clientlarImgs/client2.png"
+import c3 from "../about2/clientlarImgs/client3.png"
+import c4 from "../about2/clientlarImgs/client4.png"
+import c5 from "../about2/clientlarImgs/client5.png"
+import c6 from "../about2/clientlarImgs/client6.png"
 
 const About2 = () => {
     return (
@@ -32,7 +33,7 @@ const About2 = () => {
                 </div>
                 <div className="d-block d-lg-none container text-center">
                     <div>
-                        <img src={image} alt="" className="igm"/>
+                        <img src={image} alt="" className="igm" />
                         <p className="opacity-75">Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
                         <h2 className="about2-title py-4">Tim Smith</h2>
                         <p className="opacity-75">British Dragon Boat Racing Association</p>
